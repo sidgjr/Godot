8bfdaedac77e7ef626b14ddf053cbfe51af87c5b
sidgjr <xplouses@gmail.com>
2024-09-21 15:25:31 -0300

    ajuste

--- 2. Cursos/1. Create a procedurally generated 2D Roguelike in Godot 4/2d_roguelike/Entities/entities_scripts/player.gd
+++ 2. Cursos/1. Create a procedurally generated 2D Roguelike in Godot 4/2d_roguelike/Entities/entities_scripts/player.gd
@@ -1,16 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: int
-var input_moviment = Vector2()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func movement(delta):
-	input_moviment = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down" )
+	velocity = direction * 600
+	move_and_slide()
